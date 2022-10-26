dd83cc984cf69691e9966f24d5d800346b30eed9
Asbel <105573772+TheDivergentPicoBv@users.noreply.github.com>
2022-10-26 18:07:00 -0500

    Add files via upload

---
+++ assets/preload/stages/lukewarmland.lua
@@ -0,0 +1,12 @@
+--made with Super_Hugo's Stage Editor
+
+function onCreate()
+
+	makeLuaSprite('obj1', 'backgrounds/sansno/lukewarm-land', -2344, -1196)
+
+	setObjectOrder('obj1', 0)
+	scaleObject('obj1', 3, 3)
+
+	addLuaSprite('obj1', true)
+
+end
